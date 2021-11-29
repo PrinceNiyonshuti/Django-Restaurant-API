d820c7bff14bbc8f31775ec6c010aa7e1fe16e29
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-29 11:27:29 +0200

    deploy 01

--- Restaurant/settings.py
+++ Restaurant/settings.py
@@ -120,10 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = [os.path.join(BASE_DIR, '/images/'), ]
+django_heroku.settings(locals())
 
 
 
@@ -134,5 +135,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOW_ALL_ORIGINS = True
